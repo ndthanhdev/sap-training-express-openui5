8c5c3b9630fd437094348b57baa79a35ac30ba42
ThanhTDT <thanhtdt@outlook.com>
2017-06-28 00:50:17 +0700

    asaaa

--- chat-frontend-static/controller/ChatPanel.controller.js
+++ chat-frontend-static/controller/ChatPanel.controller.js
@@ -8,6 +8,20 @@ sap.ui.define([
             this.getView().setModel(new JSONModel({
                 message: "hello everyone"
             }), 'panel');
+
+            // attach handlers for validation errors
+            sap.ui.getCore().attachValidationError(function (evt) {
+                var control = evt.getParameter("element");
+                if (control && control.setValueState) {
+                    control.setValueState("Error");
+                }
+            });
+            sap.ui.getCore().attachValidationSuccess(function (evt) {
+                var control = evt.getParameter("element");
+                if (control && control.setValueState) {
+                    control.setValueState("None");
+                }
+            });
         },
         onShowHello: function () {
             // read msg from i18n model
@@ -20,8 +34,33 @@ sap.ui.define([
             oEventBus.publish('chatPanel', 'logout');
         },
         send: function () {
-            var oEventBus = sap.ui.getCore().getEventBus();
-            oEventBus.publish('chatPanel', 'send', this.getView().getModel('panel').getProperty("/message"));
+
+            // collect input controls
+            var view = this.getView();
+            var inputs = [
+                view.byId("inputMessage")
+            ];
+
+            // check that inputs are not empty
+            // this does not happen during data binding as this is only triggered by changes
+            jQuery.each(inputs, function (i, input) {
+                if (!input.getValue()) {
+                    input.setValueState("Error");
+                }
+            });
+
+            // check states of inputs
+            var canContinue = true;
+            jQuery.each(inputs, function (i, input) {
+                if ("Error" === input.getValueState()) {
+                    canContinue = false;
+                    return false;
+                }
+            });
+            if (canContinue) {
+                var oEventBus = sap.ui.getCore().getEventBus();
+                oEventBus.publish('chatPanel', 'send', this.getView().getModel('panel').getProperty("/message"));
+            }
         }
 
     });
